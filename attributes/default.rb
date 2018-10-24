df807c91b33c2e7b487137f08f06a98c22b54c44
Cédric Félizard <cedric@felizard.fr>
2018-10-24 09:27:22 +0000

    Allow to set bandwidth limits

--- attributes/default.rb
+++ attributes/default.rb
@@ -3,4 +3,6 @@ default['tor-ng']['torrc'] = {
   dir_port: false, # String|Bool, true means '9030'
   nickname: nil, # String|Bool, nil/false means no nickname
   exit_policy: 'reject *:* # no exits allowed',
+  relay_bandwidth_rate: nil, # Integer, in KB/s, nil means unlimited
+  relay_bandwidth_burst: nil, # Integer, in KB/s, nil means unlimited
 }
