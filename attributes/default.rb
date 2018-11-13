9049b0ac36d07e961dd8dfefa7855cd7f5c0dcb3
Cédric Félizard <cedric@felizard.fr>
2018-11-13 07:53:51 +0000

    Allow to set contact info

--- attributes/default.rb
+++ attributes/default.rb
@@ -2,6 +2,7 @@ default['tor-ng']['torrc'] = {
   or_port: true, # Array|Bool, true means default IPv4+IPv6 on port 9001
   dir_port: false, # String|Bool, true means '9030'
   nickname: nil, # String|Bool, nil/false means no nickname
+  contact_info: nil, # String, nil/false means none
   exit_policy: 'reject *:* # no exits allowed',
   relay_bandwidth_rate: nil, # Integer, in KB/s, nil means unlimited
   relay_bandwidth_burst: nil, # Integer, in KB/s, nil means unlimited
