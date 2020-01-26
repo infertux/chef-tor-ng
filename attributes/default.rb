f9c1d0d3228ce766d87b88c058089d205f07f4bf
Cédric Félizard <cedric@felizard.fr>
2020-01-26 22:11:19 +0000

    Allow to set incoming and outgoing addresses

--- attributes/default.rb
+++ attributes/default.rb
@@ -1,9 +1,11 @@
 default['tor-ng']['torrc'] = {
-  or_port: true, # Array|Bool, true means default IPv4+IPv6 on port 9001
-  dir_port: false, # String|Bool, true means '9030'
-  nickname: nil, # String|Bool, nil/false means no nickname
+  address: nil, # String, nil/false will autodetect
   contact_info: nil, # String, nil/false means none
+  dir_port: false, # String|Bool, true means '9030'
   exit_policy: 'reject *:* # no exits allowed',
-  relay_bandwidth_rate: nil, # Integer, in KB/s, nil means unlimited
+  nickname: nil, # String|Bool, nil/false means no nickname
+  or_port: true, # Array|Bool, true means default IPv4+IPv6 on port 9001
+  outbound_bind_address: nil, # String, nil/false will use default address
   relay_bandwidth_burst: nil, # Integer, in KB/s, nil means unlimited
+  relay_bandwidth_rate: nil, # Integer, in KB/s, nil means unlimited
 }
